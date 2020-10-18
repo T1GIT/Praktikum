2c08d2b3b5a20e41e1174b0dfb561e310281c479
T1MON <derbindima5@gmail.com>
2020-10-19 00:57:50 +0300

    added zoom

--- seminar_3.py
+++ seminar_3.py
@@ -8,7 +8,9 @@ class Poll(tk.Tk):
     START_INTERVAL = 500  # ms
     STICK_LEN = 100  # px (in the start)
     MARGIN = 20  # px
-    AUTOZOOM = True
+    AUTOZOOM = False
+    ZOOM_UP = 1.1
+    ZOOM_DOWN = 0.9
     # Color settings
     COLOR_MODE = "r"  # s = single; g = gradient; r = rainbow
     # Single
@@ -34,35 +36,55 @@ class Poll(tk.Tk):
         super().__init__()
         self.wm_title("Practicum of programming")
         self.size = size
-        self.center = (size // 2 + 1, size // 2 + 1)
-        self.cvs = tk.Canvas(self, width=size, height=size, bg=Poll.BG)
+        self.center = [size // 2 + 1, size // 2 + 1]
+        self.canvas = tk.Canvas(self, width=size, height=size, bg=Poll.BG)
         self.engaged_points = set()
         self.empty_points = {(0, 0)}
         self.out = False
         self.paused = False
-        self.zoom = 1
+        self.scale = 1
         self.interval = Poll.START_INTERVAL
         self.color = Poll.gradient_color(1) if Poll.COLOR_MODE == "g" else Poll.COLOR
         self.repeat = 1
         self.onstep = 0
         self.amount = 0
 
-        self.cvs.bind("<Button-1>", self.start_pause)
+        self.canvas.bind("<Button-3>", self.start_pause)
+        if not Poll.AUTOZOOM:
+            self.canvas.bind('<ButtonPress-1>', self.move_from)
+            self.canvas.bind('<B1-Motion>', self.move_to)
+            self.canvas.bind("<MouseWheel>", self.zoom)
 
     def start_pause(self, event):
+        """Change pause state"""
         self.paused = not self.paused
 
-    def to_raw(self, x, y):
-        x = self.center[0] + x * self.zoom
-        y = self.center[1] - y * self.zoom
-        return x, y
+    def move_from(self, event):
+        """Remember previous coordinates"""
+        self.canvas.scan_mark(event.x, event.y)
 
-    def to_nor(self, x, y):
-        x = (x - self.center[0]) / self.zoom
-        y = (y - self.center[1]) / -self.zoom
+    def move_to(self, event):
+        """Drag canvas to the new position"""
+        self.canvas.scan_dragto(event.x, event.y, gain=1)
+
+    def zoom(self, event):
+        """Scales content of canvas"""
+        if event.delta > 0:
+            self.canvas.scale("all", *self.center, Poll.ZOOM_UP, Poll.ZOOM_UP)
+            self.scale *= Poll.ZOOM_UP
+        elif event.delta < 0:
+            self.canvas.scale("all", *self.center, Poll.ZOOM_DOWN, Poll.ZOOM_DOWN)
+            self.scale *= Poll.ZOOM_DOWN
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    def to_raw(self, x, y):
+        """Translates coordinates to Tkinter format"""
+        x = self.center[0] + x * self.scale
+        y = self.center[1] - y * self.scale
         return x, y
 
     def get_points(self, x, y):
+        """Returns points of stick's ends"""
         orient = self.repeat % 2
         if orient > 0:
             x1 = x - Poll.HALF_STICK
@@ -75,6 +97,7 @@ class Poll(tk.Tk):
         return (x1, y1), (x2, y2)
 
     def create_stick(self, x, y):
+        """Add stick by coordinates of stick's center"""
         if (x, y) not in self.engaged_points:
             self.engaged_points.add((x, y))
             p1, p2 = self.get_points(x, y)
@@ -85,12 +108,13 @@ class Poll(tk.Tk):
                 self.engaged_points.add(point)
             for point in points - total:
                 self.empty_points.add(point)
-            self.cvs.create_line(*self.to_raw(*p1), *self.to_raw(*p2), fill=self.color)
+            self.canvas.create_line(*self.to_raw(*p1), *self.to_raw(*p2), fill=self.color)
             self.onstep += 1
             self.amount += 1
             del (points, total, p1, p2)
 
     def loop(self):
+        """One iteration"""
         if not self.paused:
             for point in set(self.empty_points):
                 if Poll.COLOR_MODE == "r":
@@ -125,9 +149,9 @@ class Poll(tk.Tk):
                 self.empty_points.discard(point)
                 self.create_stick(*point)
             if self.out and Poll.AUTOZOOM:
-                zoom = (self.size - Poll.MARGIN * 2) / (self.repeat / 2 * Poll.STICK_LEN) / self.zoom
-                self.zoom *= zoom
-                self.cvs.scale("all", *self.center, zoom, zoom)
+                zoom = (self.size - Poll.MARGIN * 2) / (self.repeat / 2 * Poll.STICK_LEN) / self.scale
+                self.scale *= zoom
+                self.canvas.scale("all", *self.center, zoom, zoom)
                 self.interval = round(poll.interval * zoom)
                 del zoom
             if Poll.COLOR_MODE == "g":
@@ -140,17 +164,20 @@ class Poll(tk.Tk):
         self.after(self.interval, self.loop)
 
     def start(self):
+        """Starts looping and shows canvas"""
         self.loop()
-        self.cvs.pack()
+        self.canvas.pack()
         self.mainloop()
     
     @staticmethod
     def rainbow_color(radius):
+        """Returns rainbow's color"""
         color = round((radius / Poll.STICK_LEN) * Poll.RAINBOW_RANGE) % len(Poll.RAINBOW_COLORS)
         return Poll.RAINBOW_COLORS[color]
         
     @staticmethod
     def gradient_color(num):
+        """Returns color from the number"""
         full_ch = 255
         half_ch = full_ch / 2
         value = (1/num) ** (1/Poll.GRADIENT_GAMMA) * full_ch
